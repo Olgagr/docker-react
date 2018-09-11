557d5b6aba2dc19bd404530c1536aa20082af90d
Olga Grabek <olga.grabek@gmail.com>
2018-09-11 20:02:42 +0200

    Change app text

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">this is my react app</p>
+        <p className="App-intro">This is changed on feature branch</p>
       </div>
     );
   }
